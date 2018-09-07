55b888c7c1f39b90dca7039f3de178e008d56921
Alexandre Abadie <alexandre.abadie@inria.fr>
2018-09-07 08:38:32 +0200

    add tests for service cli

--- iotlabwebtools/service_cli.py
+++ iotlabwebtools/service_cli.py
@@ -15,37 +15,17 @@ LOGGER = logging.getLogger("iotlabwebtools")
 LOGGER.setLevel(logging.INFO)
 
 
-def signal_handler(server, app, sig, frame):
-    # pylint:disable=unused-argument
-    """Triggered when a signal is received from system."""
-    ioloop = tornado.ioloop.IOLoop.instance()
-
-    def shutdown():
-        """Force server and ioloop shutdown."""
-        app.stop()
-        tornado.ioloop.IOLoop.current().stop()
-        if server is not None:
-            server.stop()
-        ioloop.stop()
-
-    ioloop.add_callback_from_signal(shutdown)
-    LOGGER.debug('Shuting down server (caught signal: %s)', str(sig))
-
-
-def start_application(app, port):
-    """Start a tornado application."""
-    server = app.listen(port)
-    signal.signal(signal.SIGTERM, partial(signal_handler, server, app))
-    signal.signal(signal.SIGINT, partial(signal_handler, server, app))
-
-
 def main(args=None):
     """Main function of the web application."""
-    args = args or service_cli_parser().parse_args()
+    args = service_cli_parser().parse_args(args)
     if args.debug:
         LOGGER.setLevel(logging.DEBUG)
-    start_application(WebApplication(auth_url=args.auth_url,
-                                     token=args.token), args.port)
-    LOGGER.info('Application started, listening on port %s', args.port)
-
-    tornado.ioloop.IOLoop.current().start()
+    app = WebApplication(auth_url=args.auth_url, token=args.token)
+    try:
+        app.listen(args.port)
+        LOGGER.info('Application started, listening on port %s', args.port)
+        tornado.ioloop.IOLoop.current().start()
+    except KeyboardInterrupt:
+        LOGGER.debug('Shuting down service')
+        app.stop()
+        tornado.ioloop.IOLoop.current().stop()
---
+++ iotlabwebtools/tests/test_service_cli.py
@@ -0,0 +1,49 @@
+"""iotlabwebtools service cli tests."""
+
+import sys
+import argparse
+import logging
+import unittest
+
+import mock
+
+from iotlabwebtools.service_cli import main
+from iotlabwebtools.web_application import WebApplication, DEFAULT_AUTH_URL
+
+LOGGER = logging.getLogger("iotlabwebtools")
+
+
+@mock.patch('iotlabwebtools.web_application.WebApplication.stop')
+@mock.patch('iotlabwebtools.web_application.WebApplication.listen')
+@mock.patch('iotlabwebtools.web_application.WebApplication.__init__')
+@mock.patch('tornado.ioloop.IOLoop.current')
+class ServiceCliTest(unittest.TestCase):
+
+    def test_main_service_cli_default(self, ioloop, init, listen, stop_app):
+        init.return_value = None
+        args = []
+        main(args)
+
+        # start.assert_called_once()
+        init.assert_called_with(auth_url=DEFAULT_AUTH_URL, token='')
+        listen.assert_called_with('8000')
+
+    def test_main_service_cli_args(self, ioloop, init, listen, stop_app):
+        init.return_value = None
+        auth_url_test = 'http://testhost/test'
+        port_test = '8082'
+        token_test = 'test_token'
+        args = ['--auth-url', auth_url_test, '--token', token_test,
+                '--port', port_test]
+        main(args)
+
+        # start.assert_called_once()
+        init.assert_called_with(auth_url=auth_url_test, token=token_test)
+        listen.assert_called_with(port_test)
+
+    def test_main_debug(self, ioloop, init, listen, stop_app):
+        init.return_value = None
+        args = ['--debug']
+        main(args)
+
+        assert LOGGER.getEffectiveLevel() == logging.DEBUG
