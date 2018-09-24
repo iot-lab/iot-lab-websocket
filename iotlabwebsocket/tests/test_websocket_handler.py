e611b16acd2739c703e89ecc3d28a8d155bb9162
Alexandre Abadie <alexandre.abadie@inria.fr>
2018-09-24 08:52:41 +0200

    tests: adapt websocket handler test to ssh client

--- iotlabwebsocket/tests/test_websocket_handler.py
+++ iotlabwebsocket/tests/test_websocket_handler.py
@@ -28,7 +28,7 @@ class TestWebsocketHandler(AsyncHTTPTestCase):
 
     @patch('iotlabwebsocket.handlers.http_handler._nodes')
     @gen_test
-    def test_websocket_connection(self, nodes, ws_open):
+    def test_websocket_serial_connection(self, nodes, ws_open):
         url = ('ws://localhost:{}/ws/local/123/node-1/serial'
                .format(self.api.port))
         nodes.return_value = json.dumps({'nodes': ['node-1.local']})
@@ -40,6 +40,10 @@ class TestWebsocketHandler(AsyncHTTPTestCase):
         # if handle_websocket_open is called, the connection have passed all
         # checks with success
         ws_open.assert_called_once()
+        args, _ = ws_open.call_args
+        assert len(args) == 2
+        assert isinstance(args[0], WebsocketClientHandler)
+        assert args[1] == 'serial'
 
         with patch('iotlabwebsocket.web_application'
                    '.WebApplication.handle_websocket_data') as ws_data:
@@ -48,9 +52,52 @@ class TestWebsocketHandler(AsyncHTTPTestCase):
             yield gen.sleep(0.1)
             ws_data.assert_called_once()
             args, _ = ws_data.call_args
-            assert len(args) == 2
+            assert len(args) == 3
             assert isinstance(args[0], WebsocketClientHandler)
             assert args[1] == data
+            assert args[2] == 'serial'
+            ws_handler = args[0]
+
+        with patch('iotlabwebsocket.web_application'
+                   '.WebApplication.handle_websocket_close') as ws_close:
+            connection.close()
+            yield gen.sleep(0.1)
+            ws_close.assert_called_once()
+            args, _ = ws_close.call_args
+            assert len(args) == 2
+            assert args[0] == ws_handler
+            assert args[1] == 'serial'
+
+    @patch('iotlabwebsocket.handlers.http_handler._nodes')
+    @gen_test
+    def test_websocket_ssh_connection(self, nodes, ws_open):
+        url = ('ws://localhost:{}/ws/local/123/a8-1/ssh'
+               .format(self.api.port))
+        nodes.return_value = json.dumps({'nodes': ['a8-1.local']})
+
+        connection = yield tornado.websocket.websocket_connect(
+            url, subprotocols=['token', 'token'])
+        assert connection.selected_subprotocol == 'token'
+
+        # if handle_websocket_open is called, the connection have passed all
+        # checks with success
+        ws_open.assert_called_once()
+        args, _ = ws_open.call_args
+        assert len(args) == 2
+        assert isinstance(args[0], WebsocketClientHandler)
+        assert args[1] == 'ssh'
+
+        with patch('iotlabwebsocket.web_application'
+                   '.WebApplication.handle_websocket_data') as ws_data:
+            data = "test"
+            yield connection.write_message(data)
+            yield gen.sleep(0.1)
+            ws_data.assert_called_once()
+            args, _ = ws_data.call_args
+            assert len(args) == 3
+            assert isinstance(args[0], WebsocketClientHandler)
+            assert args[1] == data
+            assert args[2] == 'ssh'
             ws_handler = args[0]
 
             # Check some websocket handler internal methods (just for coverage)
@@ -67,7 +114,10 @@ class TestWebsocketHandler(AsyncHTTPTestCase):
             connection.close(code=1000, reason="client exit")
             yield gen.sleep(0.1)
             ws_close.assert_called_once()
-            ws_close.assert_called_with(ws_handler)
+            args, _ = ws_close.call_args
+            assert len(args) == 2
+            assert args[0] == ws_handler
+            assert args[1] == 'ssh'
 
     @gen_test
     def test_websocket_connection_invalid_url(self, ws_open):
@@ -114,4 +164,13 @@ class TestWebsocketHandler(AsyncHTTPTestCase):
             _ = yield tornado.websocket.websocket_connect(
                 url, subprotocols=['user', 'token', 'token'])
         assert "HTTP 401: Unauthorized" in str(exc_info)
+        assert ws_open.call_count == 0
+
+        url = ('ws://localhost:{}/ws/local/123/node-1/ssh'  # only a8 allowed
+               .format(self.api.port))
+
+        with pytest.raises(tornado.httpclient.HTTPClientError) as exc_info:
+            _ = yield tornado.websocket.websocket_connect(
+                url, subprotocols=['token', 'token'])
+        assert "HTTP 404: Not Found" in str(exc_info)
         assert ws_open.call_count == 0
