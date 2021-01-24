5e6f5a796decc233aeeecc6835f4d0cd64b98de2
Alexandre Abadie <alexandre.abadie@inria.fr>
2021-01-24 10:44:14 +0100

    init.sh: get API_USER/API_PASSWORD variables via env

--- init.sh
+++ init.sh
@@ -4,9 +4,13 @@ set -e
 : ${API_PROTOCOL:=http}
 : ${API_HOST:=localhost}
 : ${API_PORT:=80}
+: ${API_USER:=}
+: ${API_PASSWORD:=}
 
 python3 /usr/local/bin/iotlab-websocket-service --port 8080 \
     --api-protocol ${API_PROTOCOL} \
     --api-host ${API_HOST} \
     --api-port ${API_HOST} \
+    --api-user ${API_USER} \
+    --api-password ${API_PASSWORD} \
     --log-console
