c0636973fd5e618b0efb60b72d659de20cb9b22c
Saint-Marcel Frederic <frederic.saint-marcel@inria.fr>
2022-10-07 10:31:20 +0200

    iotlabwebsocket: add port env variable

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+: ${PORT:=8080}
 : ${API_PROTOCOL:=http}
 : ${API_HOST:=localhost}
 : ${API_PORT:=80}
@@ -10,7 +11,7 @@ set -e
 export API_USER=${API_USER}
 export API_PASSWORD=${API_PASSWORD}
 
-python3 /usr/local/bin/iotlab-websocket-service --port 8080 \
+python3 /usr/local/bin/iotlab-websocket-service --port ${PORT} \
     --api-protocol ${API_PROTOCOL} \
     --api-host ${API_HOST} \
     --api-port ${API_PORT} \
